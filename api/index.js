b138b48b23a10c53e640b8664b5abc2d6bbbb00e
unknown <ravishankarray480@gmail.com>
2026-03-10 00:11:01 +0530

    fronend url add in backend

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 dbConnection();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://hrms-frontend-theta-nine.vercel.app', 
   credentials: true,               
 }));
 app.use(express.json());
