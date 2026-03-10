986b70c9ccdbfad7e00ec8d1c71ff55c4968df4b
unknown <ravishankarray480@gmail.com>
2026-03-10 11:02:43 +0530

    backend updated successfully

--- utils/pdfGenerator.js
+++ utils/pdfGenerator.js
@@ -49,7 +49,9 @@ const generatePDF = async (data) => {
 
 const browser = await puppeteer.launch({
   headless: "new",
-  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+  executablePath:
+    process.env.PUPPETEER_EXECUTABLE_PATH ||
+    "/opt/render/.cache/puppeteer/chrome/linux-145.0.7632.77/chrome-linux64/chrome",
   args: ["--no-sandbox", "--disable-setuid-sandbox"],
 });
 
